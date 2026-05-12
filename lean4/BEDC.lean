c32d2ecb1a0e69acde3bbefe62cfa313be6cde8f
Loning <aloning@gmail.com>
2026-05-13 04:27:07 +0800

    HostBridge: Church Either + List ↔ host roundtrip

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -1346,6 +1346,7 @@ import BEDC.HostBridge.HostInterpreter
 import BEDC.HostBridge.HostInterpreterArith
 import BEDC.HostBridge.ChurchBoolPairRoundTrip
 import BEDC.HostBridge.ChurchOptionRoundTrip
+import BEDC.HostBridge.ChurchEitherListRoundTrip
 /- Capstone formalizations. -/
 import BEDC.Capstone
 import BEDC.Manifest
