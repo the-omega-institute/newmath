c90444dbd117e2222f12593c70a96c9f0e92295d
AlyciaBHZ <50111876+AlyciaBHZ@users.noreply.github.com>
2026-06-10 00:41:26 +0800

    expand S^QP/M^QTP to model organisms (B.subtilis, mouse, C.elegans, Drosophila): Gram+ / mammal / nematode / insect phylogenetic breadth

--- tools/bio_reality/experiments/run_b_star_q6_protein_omics_survival_powered.py
+++ tools/bio_reality/experiments/run_b_star_q6_protein_omics_survival_powered.py
@@ -29,6 +29,10 @@ ORGANISMS = [
     "danio_rerio",
     "gallus_gallus",
     "pseudomonas_aeruginosa_pao1",
+    "bacillus_subtilis_subsp_subtilis_str_168",
+    "mus_musculus",
+    "caenorhabditis_elegans",
+    "drosophila_melanogaster",
 ]
 MIN_ORGANISMS = 2
 MIN_PROTEINS_PER_ORGANISM = 500
