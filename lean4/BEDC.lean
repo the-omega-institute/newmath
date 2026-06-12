970e2fff5ddcf7ace21e928652e48a94b8547c70
loning <loning.ma@aelf.io>
2026-06-13 01:12:25 +0800

    taste: resolve isolated_theorem fe36eecd
    Action: add_downstream_user

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -10080,6 +10080,7 @@ import BEDC.Derived.ViscositySolutionUp.TasteGate
 import BEDC.Derived.VitaliCoverUp.TasteGate
 import BEDC.Derived.VolterraIntegralOperatorUp.TasteGate
 import BEDC.Derived.VonneumannalgebraUp
+import BEDC.Derived.WLPOBoundaryUp.TasteGate
 import BEDC.Derived.WeakCompactnessUp
 import BEDC.Derived.WeakDerivativeUp.TasteGate
 import BEDC.Derived.WeakKonigBoundaryUp.TasteGate
