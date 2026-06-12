de08d447b852841119c81f39b7499a5415b523c7
loning <loning.ma@aelf.io>
2026-06-12 13:40:44 +0800

    paper-revise-paper-revise-wtjracbp: add Mittag-Leffler Bishop naming packet

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -6878,6 +6878,7 @@ import BEDC.Derived.MinkowskiInequalityUp.TasteGate
 import BEDC.Derived.MinkowskiRateGeometryUp.TasteGate
 import BEDC.Derived.MirrorSymmetryUp
 import BEDC.Derived.MirskyTheoremUp
+import BEDC.Derived.MittagLefflerBishopUp
 import BEDC.Derived.ModNUp
 import BEDC.Derived.ModelCatUp
 import BEDC.Derived.ModelCatUp.TasteGate
