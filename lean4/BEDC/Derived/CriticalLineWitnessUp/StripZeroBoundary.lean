f2a523feaabc81bcf53582ac84653be00de97301
loning <loning.ma@aelf.io>
2026-06-13 09:40:53 +0800

    Resolve critical line strip boundary name collision

--- lean4/BEDC/Derived/CriticalLineWitnessUp/StripZeroBoundary.lean
+++ lean4/BEDC/Derived/CriticalLineWitnessUp/StripZeroBoundary.lean
@@ -6,7 +6,7 @@ open BEDC.FKernel.Cont
 open BEDC.FKernel.Hist
 open BEDC.FKernel.Unary
 
-theorem CriticalLineWitnessCarrier_strip_zero_boundary {Z S M R Q H C P N stripRead : BHist} :
+theorem CriticalLineWitnessCarrier_basic_strip_zero_boundary {Z S M R Q H C P N stripRead : BHist} :
     CriticalLineWitnessCarrier Z S M R Q H C P N ->
       Cont Z S stripRead ->
         UnaryHistory Z ∧ UnaryHistory S ∧ UnaryHistory stripRead ∧ hsame H (append Z S) ∧
