2fd7887bc3704e51b8d13495fdd6510e40d75868
Loning <aloning@gmail.com>
2026-06-25 04:53:50 +0800

    zmod prime field inverse core

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -11725,6 +11725,7 @@ import BEDC.Derived.YoungSymmetrizerUp.TasteGate
 import BEDC.Derived.YoungTableauUp
 import BEDC.Derived.ZCarryUp
 import BEDC.Derived.ZCarryUp.TasteGate
+import BEDC.Derived.ZModFieldUp
 import BEDC.Derived.ZModUp
 import BEDC.Derived.ZeckendorfCarryClassifierUp
 import BEDC.Derived.ZeckendorfCarryClassifierUp.TasteGate
