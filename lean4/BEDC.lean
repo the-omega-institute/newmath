1addd8008aa374543de07b62683da6b8fdb0e43f
Loning <aloning@gmail.com>
2026-06-26 03:16:56 +0800

    formalize rh finite channel normal form

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -9668,6 +9668,7 @@ import BEDC.Derived.QuotientSoundnessBoundaryUp.TwoSurfaceExhaustion
 import BEDC.Derived.QuotientSoundnessBoundaryUp.VerdictTransportSeparation
 import BEDC.Derived.QuotientStreamRefusalUp
 import BEDC.Derived.QuotientStreamRefusalUp.TasteGate
+import BEDC.Derived.RHRoute.ChannelNormalForm
 import BEDC.Derived.RHRoute.FinitePrimeWindow
 import BEDC.Derived.RHRouteHaltingBoundaryUp.TasteGate
 import BEDC.Derived.RaabeDuhamelTestUp.TasteGate
