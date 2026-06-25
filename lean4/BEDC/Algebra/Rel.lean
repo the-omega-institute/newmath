4b26b1fe83973e086fbdd57b666d61d318533c3a
Loning <aloning@gmail.com>
2026-06-25 23:36:23 +0800

    Add parameterized quadratic integer ring

--- lean4/BEDC/Algebra/Rel.lean
+++ lean4/BEDC/Algebra/Rel.lean
@@ -4,5 +4,6 @@ import BEDC.Algebra.Rel.IntegerUp
 import BEDC.Algebra.Rel.GaussianUp
 import BEDC.Algebra.Rel.EisensteinUp
 import BEDC.Algebra.Rel.SplitComplexUp
+import BEDC.Algebra.Rel.QuadIntUp
 import BEDC.Algebra.FiniteFold
 import BEDC.Algebra.FinPerm
