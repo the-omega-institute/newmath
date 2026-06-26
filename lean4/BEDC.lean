dc189f1fb45f84b12bf4f6b13b68f3358a55cc13
Loning <aloning@gmail.com>
2026-06-27 04:59:14 +0800

    formalize kronecker symbol surface

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -7018,6 +7018,7 @@ import BEDC.Derived.JordanVariationDecompositionUp.TasteGate
 import BEDC.Derived.KKTUp
 import BEDC.Derived.KKTUp.StationarityTransport
 import BEDC.Derived.KKTUp.TasteGate
+import BEDC.Derived.KroneckerSymbolUp
 import BEDC.Derived.KSpaceUp.TasteGate
 import BEDC.Derived.KakutaniFixedPointUp.TasteGate
 import BEDC.Derived.KalmanFilterUp
