67b67d08da2e4b0e433c71afb7d2452ca1b31517
Loning <aloning@gmail.com>
2026-06-28 05:39:22 +0800

    Formalize far-end energy socket

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -9903,6 +9903,7 @@ import BEDC.Derived.RHRoute.EtaKernelDischarge
 import BEDC.Derived.RHRoute.EtaPrefix
 import BEDC.Derived.RHRoute.EventflowCertificate
 import BEDC.Derived.RHRoute.FarEndCorrectionObligation
+import BEDC.Derived.RHRoute.FarEndEnergySocket
 import BEDC.Derived.RHRoute.FarEndUnityNormalization
 import BEDC.Derived.RHRoute.FinitePrimeTowerReadout
 import BEDC.Derived.RHRoute.FinitePrimeWindow
