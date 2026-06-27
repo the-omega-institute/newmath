57e08bbfe7077b323529348f9fdcd3d22c673e96
Loning <aloning@gmail.com>
2026-06-27 13:54:20 +0800

    Perfect power exponent gcd surface

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -9359,6 +9359,7 @@ import BEDC.Derived.PeetreKFunctionalUp
 import BEDC.Derived.PellEquationUp.TasteGate
 import BEDC.Derived.PellLucasUp
 import BEDC.Derived.PellUp
+import BEDC.Derived.PerfectPowerUp
 import BEDC.Derived.PerfectSetUp.TasteGate
 import BEDC.Derived.PeriodicContinuedFractionUp.TasteGate
 import BEDC.Derived.PermutationUp
