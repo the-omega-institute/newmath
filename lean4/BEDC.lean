ae570ae5b4a81f79e1580b5f32c2f2c305ccf60e
Loning <aloning@gmail.com>
2026-06-29 12:30:11 +0800

    Thue sequence finite prefix export

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -12467,6 +12467,7 @@ import BEDC.Derived.ThomaeFunctionUp.NameCertObligations
 import BEDC.Derived.ThomaeFunctionUp.TasteGate
 import BEDC.Derived.ThreeManifoldUp
 import BEDC.Derived.ThueMorseUp
+import BEDC.Derived.ThueSequenceUp
 import BEDC.Derived.TietzeExtensionUp.TasteGate
 import BEDC.Derived.TightLocatedCutUp.TasteGate
 import BEDC.Derived.TightRealIntervalUp.TasteGate
