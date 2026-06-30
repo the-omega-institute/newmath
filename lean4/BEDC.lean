0b8787686d10780f30112828f58f3a7f21011f7b
Loning <aloning@gmail.com>
2026-06-30 09:54:52 +0800

    Add concrete zeta located zero frontier

--- lean4/BEDC.lean
+++ lean4/BEDC.lean
@@ -10161,6 +10161,7 @@ import BEDC.Derived.RHRoute.BoxCoverInduction
 import BEDC.Derived.RHRoute.BoxKernelConcrete
 import BEDC.Derived.RHRoute.CertifiedFirstZero
 import BEDC.Derived.RHRoute.ChannelNormalForm
+import BEDC.Derived.RHRoute.ConcreteZetaLocatedZero
 import BEDC.Derived.RHRoute.ConcreteZetaRationalInstance
 import BEDC.Derived.RHRoute.ConstructiveRHStatement
 import BEDC.Derived.RHRoute.ConstructiveZeta
